411884cdb1661be025652eb6fc844dbd6f6b7317
i.morgun <i.morgun@g.nsu.ru>
2024-10-06 14:55:32 +0600

    fix

--- docker_compose_create_openvpn_client.sh
+++ docker_compose_create_openvpn_client.sh
@@ -1,3 +1,3 @@
 client_name=$1
 docker-compose run --rm openvpn easyrsa build-client-full "$client_name" nopass
-docker-compose run --rm openvpn ovpn_getclient {client_name} > ~/vpn_configs/"$client_name".ovpn
+docker-compose run --rm openvpn ovpn_getclient "$client_name" > ~/vpn_configs/"$client_name".ovpn
